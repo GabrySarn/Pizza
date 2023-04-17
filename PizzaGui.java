bdecfbf0e24ec7bbbe5dc8b51a4de01b2927c63c
GabrySarn <125447671+GabrySarn@users.noreply.github.com>
2023-04-17 11:39:01 +0200

    Add files via upload

---
+++ PizzaGui.java
@@ -0,0 +1,15 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class PizzaGui extends JFrame{
+    private JButton[] btnCarni;
+    private JButton[] btnVerdure; 
+    private JButton[] btnImpasti;
+    private 
+
+
+    public PizzaGui(){
+        super("Pummarola");
+        
+    }
+}
