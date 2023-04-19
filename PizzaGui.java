49bb225f756912482dfbb7040d11374a2f201a35
GabrySarn <125447671+GabrySarn@users.noreply.github.com>
2023-04-19 23:04:28 +0200

    Mod

--- PizzaGui.java
+++ PizzaGui.java
@@ -1,15 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class PizzaGui extends JFrame implements ActionListener {
+public class PizzaGui extends JFrame{
 
-    private String folderpath = "Condimenti\\";
+    private String folderpath = "Cond\\";
 
     private ArrayList<JButton> btnCarni = new ArrayList<>();
     private ArrayList<String> carni = readFile(folderpath + "Carni.txt");
@@ -19,59 +17,84 @@ public class PizzaGui extends JFrame implements ActionListener {
     private ArrayList<String> impasti = readFile(folderpath + "Impasti.txt");
     private ArrayList<JButton> btnSalse = new ArrayList<>();
     private ArrayList<String> salse = readFile(folderpath + "Salse.txt");
+    private ArrayList<JButton> btnFormaggi = new ArrayList<>();
+    private ArrayList<String> formaggi = readFile(folderpath + "Formaggi.txt");
 
-    private JLabel lblImg;
+    private JLabel lblImg, lblTimer;
+    private JPanel pnlN, pnlC, pnlE;
+    private JButton btnForno, btnOk;
+    // fare progressivebar
 
-    private String pizza = "Pizza ";
+    private String pizza = "img\\Pizze\\Pizza";
+    private int level = 0;
 
     public PizzaGui() {
         super("Pummarola");
-        setLayout(new FlowLayout());
-        
-        for (int i = 0; i < impasti.size(); i++) {
-            btnImpasti.add(new JButton(impasti.get(i)));
-            btnImpasti.get(i).addActionListener(e->(
-                public void actionPerformed(){
-                    pizza+= e.getActionCommand();
-                    lblImg.setIcon(new ImageIcon(pizza + ".png"));
-                }
-            ));
-            add(btnImpasti.get(i));
-        }
+        setLayout(new BorderLayout());
 
-        for (int i = 0; i < salse.size(); i++) {
-            btnSalse.add(new JButton(salse.get(i)));
-            btnSalse.get(i).addActionListener(e->(
-                public void actionPerformed(){
-                      pizza += e.getActionCommand();
-                    lblImg.setIcon(new ImageIcon(pizza + ".png"));
-                }
-            ));
-            add(btnSalse.get(i));
-        }
+        pnlN = new JPanel(new FlowLayout());
+        pnlC = new JPanel(new FlowLayout());
+        pnlE = new JPanel(new GridLayout(1, 3));
 
-        for (int i = 0; i < carni.size(); i++) {
-            btnCarni.add(new JButton(carni.get(i)));
-            btnCarni.get(i).addActionListener(null);
-            add(btnCarni.get(i));
-        }
+        btnOk = new JButton("Taglia e Consegna");
+        btnOk.setIcon(new ImageIcon("img\\Cmp\\TagliaPizza.png"));
+        btnOk.setBackground(Color.GRAY);
+        btnOk.setForeground(Color.WHITE);
+        btnForno = new JButton("Cuoci in forno");
+        btnForno.setIcon(new ImageIcon("img\\Cmp\\fuoco.png"));
+        btnForno.setBackground(Color.GRAY);
+        btnForno.setForeground(Color.WHITE);
 
-        for (int i = 0; i < verdure.size(); i++) {
-            btnVerdure.add(new JButton(verdure.get(i)));
-            btnVerdure.get(i).addActionListener(null);
-            add(btnVerdure.get(i));
-        }
-        
-        
+        lblTimer = new JLabel("");
+        lblTimer.setBackground(Color.GRAY);
+        lblTimer.setForeground(Color.WHITE);
 
-        lblImg = new JLabel("Immagine");
-        add(lblImg);
+        btnImpasti = setBtn(btnImpasti, impasti);
+        btnSalse = setBtn(btnSalse, salse);
+        btnFormaggi = setBtn(btnFormaggi, formaggi);
+        btnCarni = setBtn(btnCarni, carni);
+        btnVerdure = setBtn(btnVerdure, verdure);
 
-        pack();
+        lblImg = new JLabel();
+
+        pnlC.setBackground(Color.lightGray);
+        pnlC.add(lblImg);
+
+        pnlN.add(btnImpasti.get(level));
+        pnlN.add(btnSalse.get(level));
+        pnlN.add(btnCarni.get(level));
+        pnlN.add(btnVerdure.get(level));
+        pnlN.add(btnFormaggi.get(level));
+
+        pnlE.setBackground(Color.GRAY);
+        pnlE.add(btnForno);
+        pnlE.add(lblTimer);
+        pnlE.add(btnOk);
+
+        add(pnlN, BorderLayout.NORTH);
+        add(pnlC, BorderLayout.CENTER);
+        add(pnlE, BorderLayout.SOUTH);
+
+        setSize(700, 350);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
+    }
+
+    public ArrayList<JButton> setBtn(ArrayList<JButton> btn, ArrayList<String> ing) {
+        for (int i = 0; i < ing.size(); i++) {
+            btn.add(new JButton());
+            btn.get(i).setActionCommand(ing.get(i));
+            btn.get(i).setIcon(new ImageIcon("img\\Cond\\" + ing.get(i) + ".png"));
+            btn.get(i).addActionListener(e -> {
+                pizza += " " + e.getActionCommand();
+                System.out.println(pizza + ".png");
+                lblImg.setIcon(new ImageIcon(pizza + ".png"));
+            });
+        }
+
+        return btn;
     }
 
     public static ArrayList<String> readFile(String percorso) {
