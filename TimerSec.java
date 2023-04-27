e5442b175a524ba1f5ace382c3746ad335fefff1
GabrySarn <125447671+GabrySarn@users.noreply.github.com>
2023-04-27 19:50:30 +0200

    unione

---
+++ TimerSec.java
@@ -0,0 +1,34 @@
+import javax.swing.JLabel;
+
+public class TimerSec extends Thread {
+    private JLabel txt;
+
+    public TimerSec(JLabel txt) {
+        this.txt = txt;
+    }
+
+    public void run() {
+        synchronized (txt) {
+            int i = Integer.parseInt(txt.getText());
+
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            while (i > 1) {
+                i = Integer.parseInt(txt.getText()) - 1;
+                txt.setText("" + i);
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            txt.setText("Prontaaa");
+        }
+
+    }
+
+}
