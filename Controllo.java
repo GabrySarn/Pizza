bdd45e27cd3b58c0b8d42296fb57538fbb52caad
GabrySarn <125447671+GabrySarn@users.noreply.github.com>
2023-05-16 12:59:19 +0200

    csw

--- Controllo.java
+++
@@ -1,8 +0,0 @@
-public class Controllo{
-    public static void main(String[] args) {
-        //new Consegna(null, null, null);
-        //System.out.println(new PizzaDB().leggiDB());
-        new PizzaGui(null, null);
-        
-    }
-}
